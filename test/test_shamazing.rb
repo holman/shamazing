fdb31214c2cca29e4f723ad676cddb043bd73986
Zach Holman <zach@zachholman.com>
2012-03-27 22:27:38 -0700

    Command Line Interface

--- test/test_shamazing.rb
+++ test/test_shamazing.rb
@@ -17,4 +17,21 @@ class TestShamazing < Test::Unit::TestCase
   def test_integer
     assert_equal 20191041554, Shamazing.integer(@sha)
   end
+
+  def test_cli
+    output = `bin/shamazing #{@sha}`.chomp
+
+    assert_match /Longest string: cddfaedaea/, output
+    assert_match /Longest integer: 20191041554/, output
+  end
+
+  def test_cli_string
+    assert_equal 'cddfaedaea', `bin/shamazing #{@sha} -s`.chomp
+    assert_equal 'cddfaedaea', `bin/shamazing #{@sha} --string`.chomp
+  end
+
+  def test_cli_integer
+    assert_equal '20191041554', `bin/shamazing #{@sha} -i`.chomp
+    assert_equal '20191041554', `bin/shamazing #{@sha} --integer`.chomp
+  end
 end
